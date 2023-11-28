fa732e2afb39531e90d99337af1d928e565c3a6c
Hanyu Zhuang <hankzzz@hanyus-air.home>
2023-11-27 23:32:19 -0500

    updated the criteriaviewmodel to connect with the loginview presenter

--- src/search/service/search_criteria/interface_adapter/SearchCriteriaViewModel.java
+++ src/search/service/search_criteria/interface_adapter/SearchCriteriaViewModel.java
@@ -1,4 +1,32 @@
 package search.service.search_criteria.interface_adapter;
 
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+
 public class SearchCriteriaViewModel {
+    private SearchCriteriaState state;
+    private final PropertyChangeSupport support;
+
+    public SearchCriteriaViewModel() {
+        this.state = new SearchCriteriaState();
+        this.support = new PropertyChangeSupport(this);
+    }
+
+    public SearchCriteriaState getState() {
+        return state;
+    }
+
+    public void setState(SearchCriteriaState state) {
+        this.state = state;
+    }
+
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
 }
