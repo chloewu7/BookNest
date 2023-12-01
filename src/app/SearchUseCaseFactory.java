2ebb736f7e52511facdbe7959f33df01c6ca5e86
chloewu7 <113572106+chloewu7@users.noreply.github.com>
2023-12-01 18:50:36 -0500

    Modification to main; Implement SearchUseCaseFactory

--- src/app/SearchUseCaseFactory.java
+++ src/app/SearchUseCaseFactory.java
@@ -1,4 +1,28 @@
 package app;
 
+import search.data_access.SearchDataAccessObject;
+import search.entity.NewResponseFactory;
+import search.entity.ResponseFactory;
+import search.service.SearchDataAccessInterface;
+import search.service.SearchInputData;
+import search.service.SearchInteractor;
+import search.service.SearchOutputBoundary;
+import search.service.interface_adapter.SearchPresenter;
+import search.service.interface_adapter.SearchViewModel;
+
 public class SearchUseCaseFactory {
+
+    private SearchUseCaseFactory(){}
+
+
+    public static SearchInteractor createSearchInteractor(SearchViewModel searchViewModel) {
+        SearchDataAccessInterface searchDAO = new SearchDataAccessObject();
+        SearchOutputBoundary searchPresenter = new SearchPresenter(searchViewModel);
+        ResponseFactory responseFactory = new NewResponseFactory();
+        SearchInteractor searchInteractor = new SearchInteractor(searchDAO, searchPresenter, responseFactory);
+
+        return searchInteractor;
+
+    }
 }
+
