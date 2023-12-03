f376669fae6fc95c4d1997dfb969b0d72e64ceda
chloewu7 <113572106+chloewu7@users.noreply.github.com>
2023-12-02 22:21:17 -0500

    Add ActionListener for commentButton

--- src/search/service/interface_adapter/SearchState.java
+++ src/search/service/interface_adapter/SearchState.java
@@ -6,6 +6,10 @@ public class SearchState {
     private List<Book> books;
     private String notFound = null;
     private String keywordError = null;
+    public Book collectedBook;
+
+    public String commentBookTitle;
+
     public SearchState(SearchState copy){
         books = copy.books;
         notFound = copy.notFound;
@@ -15,6 +19,10 @@ public class SearchState {
     public List<Book> getBooks(){return books;}
     public String getNotFound(){return notFound;}
     public String getKeywordError(){return keywordError;}
+    public String getCommentBookTitle(){return commentBookTitle;}
+    public Book getCollectedBook(){return collectedBook;}
+    public void setCommentBookTitle(String title){this.commentBookTitle = title;}
+    public void setCollectedBook(Book collectedBook){this.collectedBook = collectedBook;}
     public void setBooks(List<Book> books){this.books = books;}
     public void setNotFound(String notFound){this.notFound = notFound;}
     public void setKeywordError(String keywordError){this.keywordError = keywordError;}
