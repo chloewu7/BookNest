615a96dde577b3c15b777aef63e7eaacf05bcb01
GabbyWang07 <gabby.wang@mail.utoronto.ca>
2023-12-04 23:55:11 -0500

    implemented Test for Signup service with 2% line coverage for overall code base and 100% line coverage for use case interactor.

--- test/user_manage/service/signup/SignupInteractorTest.java
+++ test/user_manage/service/signup/SignupInteractorTest.java
@@ -1,12 +1,122 @@
 package user_manage.service.signup;
 
 import org.junit.jupiter.api.Test;
+import user_manage.entity.CommonUserFactory;
+import user_manage.entity.User;
+import user_manage.entity.UserFactory;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SignupInteractorTest {
+    public final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    private class UserExistDataAccessStub implements SignupDataAccessInterface {
+
+
+        @Override
+        public boolean existsByName(String username) {
+            return true;
+        }
+
+        @Override
+        public void saveNewUser(User newuser) {
+
+        }
+    }
+
+    private class MockUserExistPresenter implements SignupOutputBoundary {
+
+        @Override
+        public void prepareLoginView(SignupOutputData response) {
+            fail("show success screen when it suppose to tell the user they can sign up with the existing username");
+        }
+
+        @Override
+        public void prepareFailView(String error) {
+            assertEquals("User already exists.", error);
+        }
+    }
+
+    private class MockURepeatedPasswordPresenter implements SignupOutputBoundary {
+
+        @Override
+        public void prepareLoginView(SignupOutputData response) {
+            fail("show success screen when it suppose to tell the user they can sign up with the existing username");
+        }
+
+        @Override
+        public void prepareFailView(String error) {
+            assertEquals("Repeat password doesn't match.", error);
+        }
+    }
+    private class MockUserFactory implements UserFactory{
+
+        @Override
+        public User create(String name, String password, LocalDateTime ltd) {
+            assertEquals("username", name);
+            assertEquals("password", password);
+            return null;
+        }
+    }
 
     @Test
-    void execute() {
+    void executeWhenUserExistsByName() {
+        UserExistDataAccessStub UserExistDataAccessStub = new UserExistDataAccessStub();
+        MockUserExistPresenter mockUserExistPresenter = new MockUserExistPresenter();
+        UserFactory userFactory = new CommonUserFactory();
+        SignupInteractor signupInteractor = new SignupInteractor(UserExistDataAccessStub,
+                mockUserExistPresenter, userFactory);
+        SignupInputData signupInputData = new SignupInputData("existing username", "p", "p");
+        signupInteractor.execute(signupInputData);
+        MockURepeatedPasswordPresenter mockURepeatedPasswordPresenter = new MockURepeatedPasswordPresenter();
+        MockUserDataAccess mockUserDataAccess = new MockUserDataAccess();
+        SignupInteractor signupInteractor2 = new SignupInteractor(mockUserDataAccess, mockURepeatedPasswordPresenter, userFactory);
+        SignupInputData signupInputData2 = new SignupInputData("name", "p","rp");
+        signupInteractor2.execute(signupInputData2);
+    }
+
+    private class MockUserDataAccess implements SignupDataAccessInterface{
+
+        @Override
+        public boolean existsByName(String username) {
+            return false;
+        }
+
+        @Override
+        public void saveNewUser(User newuser) {
+            assertEquals("username", newuser.getName());
+            assertEquals("password", newuser.getPassword());
+            assertEquals(LocalDateTime.now().format(formatter), newuser.getCreationTime().format(formatter));
+        }
+    }
+
+    private class MockSignupPresenter implements SignupOutputBoundary{
+
+        @Override
+        public void prepareLoginView(SignupOutputData response) {
+            assertEquals("username", response.getUsername());
+            assertEquals(String.valueOf(LocalDateTime.now().format(formatter)), response.getCreationTime().substring(0, 10));
+        }
+
+        @Override
+        public void prepareFailView(String error) {
+            fail("show fail screen when it suppose to successfully create new user");
+        }
+    }
+
+    @Test
+    void executeWhenUserCanBeCreated() {
+        MockUserDataAccess mockUserDataAccess = new MockUserDataAccess();
+        MockSignupPresenter mockSignupPresenter = new MockSignupPresenter();
+        UserFactory userFactory = new CommonUserFactory();
+        SignupInteractor signupInteractor = new SignupInteractor(mockUserDataAccess,
+                mockSignupPresenter, userFactory);
+        SignupInputData signupInputData2 = new SignupInputData("username", "password", "password");
+        signupInteractor.execute(signupInputData2);
+
     }
 }
--- test/user_manage/service/signup/interface_adapter/SignupPresenterTest.java
+++ test/user_manage/service/signup/interface_adapter/SignupPresenterTest.java
@@ -1,16 +1,73 @@
 package user_manage.service.signup.interface_adapter;
 
+import interface_adapter.ViewManagerModel;
 import org.junit.jupiter.api.Test;
+import user_manage.service.login.interface_adapter.LoginState;
+import user_manage.service.login.interface_adapter.LoginViewModel;
+import user_manage.service.signup.SignupOutputData;
+
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SignupPresenterTest {
+    public final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    @Test
-    void prepareLoginView() {
+    //test signup success cases
+    private class MockSuccessSignupViewModel extends SignupViewModel {
+        public void setState(SignupState newState) {
+            assertEquals("name", newState.getUsername());
+            assertEquals("p", newState.getPassword());
+            assertEquals("p", newState.getRepeatPassword());
+        }
+    }
+
+    private class MockSuccessViewManagerModel extends ViewManagerModel{
+        public void setActiveView(String activeView) {
+            assertEquals(activeView, "log in");
+        }
+    }
+
+    private class MockLoginViewModel extends LoginViewModel{
+        public void setState(LoginState newState) {
+            assertEquals("name", newState.getUsername());
+        }
+
     }
 
     @Test
-    void prepareFailView() {
+    void testPrepareSuccessView() {
+        MockSuccessSignupViewModel mockSuccessSignupViewModel = new MockSuccessSignupViewModel();
+        MockSuccessViewManagerModel mockSuccessViewManagerModel = new MockSuccessViewManagerModel();
+        MockLoginViewModel mockLoginViewModel = new MockLoginViewModel();
+
+        SignupPresenter signupPresenter = new SignupPresenter(mockSuccessViewManagerModel, mockSuccessSignupViewModel, mockLoginViewModel);
+        SignupOutputData signupOutputData = new SignupOutputData("name", String.valueOf(LocalDateTime.now()), false);
+        signupPresenter.prepareLoginView(signupOutputData);
+    }
+
+    //test signup fail cases
+
+    private class MockFailViewManagerModel extends ViewManagerModel {
+        public void setActiveView(String activeView) {
+            assertEquals("sign up", activeView);
+        }
+    }
+
+    private class MockfailViewModel extends SignupViewModel{
+        public void setState(SignupState newState) {
+            assertEquals("error", newState.getUsernameError());
+        }
+    }
+
+    @Test
+    void testPrepareNoReviewView() {
+        MockfailViewModel mockNameErrorViewModel = new MockfailViewModel();
+        MockFailViewManagerModel mockFailViewManagerModel = new MockFailViewManagerModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        SignupPresenter nameErrorPresenter = new SignupPresenter(mockFailViewManagerModel, mockNameErrorViewModel, loginViewModel);
+        nameErrorPresenter.prepareFailView("error");
     }
 }
